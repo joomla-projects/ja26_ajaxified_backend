a91f2d13b30265a8cd4216d582137c1dc827ddce
Adarsh Dubey <adarshdubeyisro03@gmail.com>
2026-06-20 00:18:58 +0530

    cs

--- administrator/templates/atum/index.php
+++ administrator/templates/atum/index.php
@@ -202,9 +202,9 @@ $statusModules = LayoutHelper::render('status', ['modules' => 'status']);
             <div id="subhead-container" class="subhead mb-3">
                 <div class="row">
                     <div class="col-md-12">
-                        <?start name="toolbar"?>
+                        <?php echo '<?start name="toolbar"?>'; ?>
                         <jdoc:include type="modules" name="toolbar" style="none" />
-                        <?end?>
+                        <?php echo '<?end?>'; ?>
                     </div>
                 </div>
             </div>
