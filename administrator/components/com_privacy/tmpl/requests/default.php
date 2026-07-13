104c400bd4027ef9527ac540d5238c5965b35903
Adarsh Dubey <adarshdubeyisro03@gmail.com>
2026-07-13 14:18:09 +0530

    refresh only list views

--- administrator/components/com_privacy/tmpl/requests/default.php
+++ administrator/components/com_privacy/tmpl/requests/default.php
@@ -22,8 +22,7 @@ use Joomla\CMS\String\PunycodeHelper;
 
 /** @var \Joomla\CMS\WebAsset\WebAssetManager $wa */
 $wa = $this->getDocument()->getWebAssetManager();
-$wa->useScript('table.columns')
-    ->useScript('multiselect');
+$wa->useScript('multiselect');
 
 $user      = $this->getCurrentUser();
 $listOrder = $this->escape($this->state->get('list.ordering'));
@@ -35,8 +34,12 @@ $urgentRequestDate->sub(new DateInterval('P' . $this->urgentRequestAge . 'D'));
 
 ?>
 <form action="<?php echo Route::_('index.php?option=com_privacy&view=requests'); ?>" method="post" name="adminForm" id="adminForm">
+    <?php echo HTMLHelper::_('progressiveSynchronization.start', 'j-main-container'); ?>
     <div id="j-main-container">
-        <?php echo LayoutHelper::render('joomla.searchtools.default', ['view' => $this]); ?>
+        <?php
+        echo LayoutHelper::render('joomla.searchtools.default', ['view' => $this]);
+        echo LayoutHelper::render('joomla.system.toggle-columns', ['table-selector' => '#requestList']);
+        ?>
         <?php if (empty($this->items)) : ?>
             <div class="alert alert-info">
                 <span class="icon-info-circle" aria-hidden="true"></span><span class="visually-hidden"><?php echo Text::_('INFO'); ?></span>
@@ -125,4 +128,5 @@ $urgentRequestDate->sub(new DateInterval('P' . $this->urgentRequestAge . 'D'));
 
         <?php echo $this->filterForm->renderControlFields(); ?>
     </div>
+    <?php echo HTMLHelper::_('progressiveSynchronization.end'); ?>
 </form>
